a6e906ff4476f82b3f68569eeccb3b16009f421f
kalashjain23 <kalashjain513@gmail.com>
2022-08-12 17:58:54 +0530

    added code for giving manager a bonus of 10%

---
+++ Home-Work/August_5/ManagerBonus.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+
+struct Date{
+    int date;
+    std::string month;
+    int year;
+};
+
+struct Employee{
+    std::string name;
+    int salary;
+    Date birth;
+    Date joining;
+    std::string designation;
+};
+Employee emp[3];
+
+void get_data(Employee* employee){
+    int n;
+    std::string s;
+
+    std::cout << "Enter name: ";
+    std::cin >> s;
+    employee->name = s;
+
+    std::cout << "Enter salary: ";
+    std::cin >> n;
+    employee->salary = n;
+
+    std::cout << "Enter birth date: ";
+    std::cin >> n;
+    employee->birth.date = n;
+    std::cout << "Enter birth month: ";
+    std::cin >> s;
+    employee->birth.month = s;
+    std::cout << "Enter birth year: ";
+    std::cin >> n;
+    employee->birth.year = n;
+
+    std::cout << "Enter joining date: ";
+    std::cin >> n;
+    employee->joining.date = n;
+    std::cout << "Enter joining month: ";
+    std::cin >> s;
+    employee->joining.month = s;
+    std::cout << "Enter joining year: ";
+    std::cin >> n;
+    employee->joining.year = n;
+
+    std::cout << "Enter designation: ";
+    std::cin >> s;
+    employee->designation = s;
+}
+
+void get_date(Date* today){
+    int date, year;
+    std::string month;
+    std::cin >> date >> month >> year;
+    today->date = date;
+    today->month = month;
+    today->year = year;
+}
+
+int main(){
+    Date today;
+    get_date(&today);
+
+    for(Employee employee:emp){
+        get_data(&employee);
+        if(today.year - employee.joining.year >= 3 && employee.designation == "Manager"){
+            employee.salary += (employee.salary)/10;
+            std::cout << "After receiving 10% bonus, " << employee.name <<"'s salary is " << employee.salary << "\n";
+        }
+    }
+}
