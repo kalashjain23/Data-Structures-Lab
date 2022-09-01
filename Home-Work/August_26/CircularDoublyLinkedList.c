8a2c27ded0ba30c8ac63ed8f88cb8d05cc8ca4c3
kalashjain23 <kalashjain513@gmail.com>
2022-09-02 00:05:23 +0530

    code for creating, inserting element and printing circular doubly linked list

---
+++ Home-Work/August_26/CircularDoublyLinkedList.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct DoublyLinkedList{
+    int val;
+    struct DoublyLinkedList* prev;
+    struct DoublyLinkedList* next;
+}*head, *first, *list;
+typedef struct DoublyLinkedList Node;
+
+void insert(int val){
+    Node* temp = (Node*)(malloc(sizeof(Node)));
+    temp->val = val;
+    temp->next = head;
+    temp->prev = list;
+
+    list->next = temp;
+    list = list->next;
+}
+
+int main(){
+
+    head = NULL;
+    first = (Node*)(malloc(sizeof(Node)));
+    first->val = 20;
+    first->next = NULL;
+    first->prev = NULL;
+
+    head = first;
+    list = first;
+
+    int n, val;
+    while(1){
+        printf("Do you want items (1 / 0): ");
+        scanf("%d",&n);
+        if(n == 1){
+            printf("Value: ");
+            scanf("%d",&val);
+            insert(val);
+        }
+        else break;
+    }
+
+    printf("The list: ");
+    Node* temp = head;
+    
+    do{
+        printf("%d ", temp->val);
+        temp = temp->next;
+    } while(temp != head);
+
+}
